4128f4622f85a59d4348736a7226c28a77135949
ofthegoats <47647338+ofthegoats@users.noreply.github.com>
2022-07-14 19:43:51 +0100

    Restarted project

---
+++ src/callbacks.cpp
@@ -0,0 +1,45 @@
+#define GLFW_INCLUDE_NONE
+
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+#include <iostream>
+#include <queue>
+#include <utility>
+
+#include "callbacks.hpp"
+
+void framebufferSizeCallback(GLFWwindow *, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void keyCallback(GLFWwindow *window, int key, int, int action, int) {
+  if ((key == GLFW_KEY_Q || key == GLFW_KEY_ESCAPE) && action == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
+
+extern std::queue<std::queue<std::pair<double, double>>> drawQueue;
+extern std::queue<bool> strokeFinishQueue;
+bool drawing = false;
+
+void mouseButtonCallback(GLFWwindow*, int button, int action, int mods) {
+  if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+    std::cout << "starting new stroke" << std::endl;
+    drawing = true;
+    strokeFinishQueue.push(false);
+    drawQueue.push(std::queue<std::pair<double, double>>());
+  } else if (action == GLFW_RELEASE) {
+    std::cout << "finished stroke" << std::endl;
+    drawing = false;
+    strokeFinishQueue.back() = true;
+  }
+}
+
+void cursorPositionCallback(GLFWwindow *window, double xpos, double ypos) {
+  if (drawing) {
+    std::cout << "position callback pushing" << std::endl;
+    int width, height;
+    glfwGetWindowSize(window, &width, &height);
+    drawQueue.front().push({2 * (xpos / width) - 1, 1 - 2 * (ypos / height)});
+  }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,129 +1,90 @@
 #define GLFW_INCLUDE_NONE
-#define GLFW_EXPOSE_NATIVE_X11
-#define EASYTAB_IMPLEMENTATION
+
 #include <GLFW/glfw3.h>
-#include <GLFW/glfw3native.h>
-#include <X11/Xlib.h>
-#include <easytab.h>
 #include <glad/glad.h>
 #include <iostream>
+#include <queue>
+#include <utility>
+#include <assert.h>
 
-void framebuffer_size_callback(GLFWwindow*, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}
+#include "callbacks.hpp"
 
-void process_input(GLFWwindow* window)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-}
+// queue of strokes containing vertices to draw to
+std::queue<std::queue<std::pair<double, double>>> drawQueue;
+std::queue<bool> strokeFinishQueue;
 
-int main()
-{
-    // set up GLFW and OpenGL window
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_SAMPLES, 32);
-    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    // HACK using immediate mode, which is inefficient
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "drawer", NULL, NULL);
-    if (window == NULL) {
-        std::cerr << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return 1;
+int main() {
+  // glfw setup
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_SAMPLES, 4);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+
+  // window-specific hints
+  glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+  glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
+  glfwWindowHint(GLFW_FLOATING, GLFW_TRUE);
+
+  // make window
+  GLFWwindow *window = glfwCreateWindow(1200, 800, "noter", NULL, NULL);
+  if (window == NULL) {
+    std::cerr << "Failed to create GLFW window\n";
+    return 1;
+  }
+  glfwMakeContextCurrent(window);
+  // load glad
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cerr << "Failed to initialize GLAD" << std::endl;
+    return 1;
+  }
+  glViewport(0, 0, 1200, 800);
+
+  glfwSetWindowPos(window, 520, 200);
+  glfwSetKeyCallback(window, keyCallback);
+  glfwSetCursorPosCallback(window, cursorPositionCallback);
+  glfwSetMouseButtonCallback(window, mouseButtonCallback);
+  glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+  // enable antialiasing - should be on by default on newer hardware
+  glEnable(GL_MULTISAMPLE_BIT);
+
+  // background for front and back buffer
+  glClearColor((float)29/255, (float)32/255, (float)33/255, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT);
+  glfwSwapBuffers(window);
+  glClear(GL_COLOR_BUFFER_BIT);
+
+
+  // for now, only one drawing colour and width is available
+  glColor3f((float)204 / 255, (float)36 / 255, (float)29 / 255);
+  glLineWidth(3);
+
+  while (!glfwWindowShouldClose(window)) {
+    assert(drawQueue.size() == strokeFinishQueue.size());
+
+    // drawing in immediate mode
+    glBegin(GL_LINE_STRIP);
+    // draw as long as we can
+    while (drawQueue.front().size() >= 2) {
+      glVertex2d(drawQueue.front().front().first, drawQueue.front().front().second);
+      drawQueue.front().pop();
+      glVertex2d(drawQueue.front().front().first, drawQueue.front().front().second);
+      drawQueue.front().pop();
     }
-    glfwMakeContextCurrent(window);
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cerr << "Failed to initialize GLAD" << std::endl;
-        return 1;
+    // if we're done, pop the stroke
+    if (drawQueue.front().empty() && strokeFinishQueue.front()) {
+      std::cout << "popping stroke" << std::endl;
+      drawQueue.pop();
+      strokeFinishQueue.pop();
     }
-    glViewport(0, 0, 800, 600);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glEnd();
 
-    // Get native X11 display and window for easytab
-    Display* x11Display = glfwGetX11Display();
-    Window x11Window = glfwGetX11Window(window);
-    if (EasyTab_Load(x11Display, x11Window) != EASYTAB_OK) {
-        std::cerr << "Failed to load EasyTab" << std::endl;
-    }
-
-    // TODO set up vertices, buffers etc, using modern opengl
-
-    glEnable(GL_MULTISAMPLE_BIT);
-
-    glClear(GL_COLOR_BUFFER_BIT);
     glfwSwapBuffers(window);
-    glDrawBuffer(GL_FRONT_AND_BACK);
+    glfwPollEvents();
+  }
 
-    // x, y, pressure
-    double cursor_last_pos[3];
-    glfwGetCursorPos(window, &cursor_last_pos[0], &cursor_last_pos[1]);
-    cursor_last_pos[2] = 0;
-
-    double const base_radius = 0.006;
-
-    while (!glfwWindowShouldClose(window)) {
-        process_input(window);
-
-        // normalised x and y position of cursor
-        double posx, posy;
-        // width and height of the window used to normalise posx,posy
-        int width, height;
-        glfwGetWindowSize(window, &width, &height);
-        // pressure in [0, 1]
-        double pressure = 0;
-
-        XEvent xevent;
-        XNextEvent(x11Display, &xevent);
-        if (EasyTab_HandleEvent(&xevent) == EASYTAB_OK) {
-            posx = 2 * (double)EasyTab->PosX / width - 1;
-            posy = -2 * (double)EasyTab->PosY / height + 1;
-            pressure = (double)EasyTab->Pressure;
-        } else {
-            glfwGetCursorPos(window, &posx, &posy);
-            posx = 2 * posx / width - 1;
-            posy = -2 * posy / height + 1;
-            if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
-                pressure = 0.5;
-        }
-
-#ifndef NDEBUG
-        std::cout << "x = " << posx << " y = " << posy << " p = " << pressure << std::endl;
-#endif
-
-        // TODO render here
-        // HACK should use a sprite for the cursor, or nothing
-        glColor3f(1.0f, 0.0f, 0.0f);
-        glBegin(GL_POLYGON);
-        glVertex2d(posx - base_radius * pressure, posy);
-        glVertex2d(posx, posy + base_radius * pressure);
-        glVertex2d(posx + base_radius * pressure, posy);
-        glVertex2d(posx, posy - base_radius * pressure);
-        glEnd();
-        glBegin(GL_POLYGON);
-        glVertex2d(cursor_last_pos[0] - base_radius * cursor_last_pos[2], cursor_last_pos[1]);
-        glVertex2d(cursor_last_pos[0] + base_radius * cursor_last_pos[2], cursor_last_pos[1]);
-        glVertex2d(posx + base_radius * pressure, posy);
-        glVertex2d(posx - base_radius * pressure, posy);
-        glEnd();
-        glBegin(GL_POLYGON);
-        glVertex2d(cursor_last_pos[0], cursor_last_pos[1] - base_radius * cursor_last_pos[2]);
-        glVertex2d(cursor_last_pos[0], cursor_last_pos[1] + base_radius * cursor_last_pos[2]);
-        glVertex2d(posx, posy + base_radius * pressure);
-        glVertex2d(posx, posy - base_radius * pressure);
-        glEnd();
-
-        cursor_last_pos[0] = posx;
-        cursor_last_pos[1] = posy;
-        cursor_last_pos[2] = pressure;
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-    glfwTerminate();
-    EasyTab_Unload(x11Display);
-    return 0;
+  glfwTerminate();
+  return 0;
 }
+
